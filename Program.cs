1d72ccd025e93bc6507aa0e6770dbd20e2aa2599
FernaCaroline <160563604+FernaCaroline@users.noreply.github.com>
2025-06-13 11:56:59 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,28 @@
+using CadastroFuncionariosApi.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Adiciona o serviço do banco de dados
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
